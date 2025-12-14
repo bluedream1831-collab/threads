c3c6ced4e950eb1b00ca2d0ac1e6b22a22f8094b
bluedream1831-collab <bluedream1831@gmail.com>
2025-12-14 22:30:07 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,11 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    // 這裡將 Vercel 環境變數中的 API_KEY 注入到前端代碼中的 process.env.API_KEY
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
+  },
 });
